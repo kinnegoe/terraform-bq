7eb6d10d10c3d87f2a1e3036241f3075f30a8903
kinnegoe <stuff4jason@gmail.com>
2017-05-24 16:09:40 +0800

    Update terraform-bq-table.tf

--- terraform-bq-table.tf
+++ terraform-bq-table.tf
@@ -1,3 +1,15 @@
+// Configure the Google Cloud provider
+provider "google" {
+  credentials = "${file("JasonM-ML-Project888-0dec86ec8b3b.json")}"
+  project     = "jasonm-ml-project888"
+  region      = "us-central1"
+}
+
+// Create a new instance
+resource "google_compute_instance" "default" {
+  # ...
+}
+
 resource "google_bigquery_dataset_terraform" "default" {
   dataset_id                  = "test_terraform"
   friendly_name               = "test_terraform"
