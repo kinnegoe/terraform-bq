949b1019015355aeb1c924b83e848a413cc1ad97
kinnegoe <stuff4jason@gmail.com>
2017-05-29 10:15:13 +0800

    Update gcp-asia-southeast1.tf

--- gcp-asia-southeast1.tf
+++ gcp-asia-southeast1.tf
@@ -2,7 +2,7 @@
 # Google Cloud Platform
 #####################################################################
 provider "google" {  
-    credentials = "${file("JasonM-ML-Project888-0dec86ec8b3b.json")}"
+    credentials = "${file("../JasonM-ML-Project888-0dec86ec8b3b.json")}"
     project = "jasonm-ml-project888"
     region = "asia-southeast1"
 }
