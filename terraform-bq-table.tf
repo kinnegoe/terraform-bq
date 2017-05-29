4b1cabf7248311c6e7a0faee07399d0c1daaf063
kinnegoe <stuff4jason@gmail.com>
2017-05-29 10:53:47 +0800

    Update terraform-bq-table.tf

--- terraform-bq-table.tf
+++ terraform-bq-table.tf
@@ -21,7 +21,7 @@ resource "google_bigquery_dataset" "default" {
 
 resource "google_bigquery_table" "default" {
   //dataset_id = "${google_bigquery_dataset.default.id}"
-  dataset_id = "test-terraform-bq}"
+  dataset_id = "test-terraform-bq"
   table_id   = "test"
 
   time_partitioning {
