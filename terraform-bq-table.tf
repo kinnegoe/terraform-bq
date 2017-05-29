49803c8274c0fc1f3f702f4e9274467a56be2e06
kinnegoe <stuff4jason@gmail.com>
2017-05-29 10:44:16 +0800

    Update terraform-bq-table.tf

--- terraform-bq-table.tf
+++ terraform-bq-table.tf
@@ -5,15 +5,31 @@ provider "google" {
   region      = "us-central1"
 }
 
+
 resource "google_bigquery_dataset" "default" {
-  dataset_id                  = "test_terraform"
-  friendly_name               = "test_terraform"
-  description                 = "This is a test description - terraform"
-  location                    = "US"
+  dataset_id                  = "test"
+  friendly_name               = "test"
+  description                 = "This is a test description"
+  location                    = "EU"
   default_table_expiration_ms = 3600000
 
   labels {
     env = "default"
   }
- //schema = "${file("terraform-bq-schema.json")}"
+}
+
+
+resource "google_bigquery_table" "default" {
+  dataset_id = "${google_bigquery_dataset.default.id}"
+  table_id   = "test"
+
+  time_partitioning {
+    type = "DAY"
+  }
+
+  labels {
+    env = "default"
+  }
+
+  schema = "${file("bq-schema.json")}"
 }
