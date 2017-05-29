f5b01697ad469cb7aba1360f2c47529ceba6f367
kinnegoe <stuff4jason@gmail.com>
2017-05-29 10:14:41 +0800

    Update terraform-bq-table.tf

--- terraform-bq-table.tf
+++ terraform-bq-table.tf
@@ -1,6 +1,6 @@
 // Configure the Google Cloud provider
 provider "google" {
-  credentials = "${file("../JasonM-ML-Project888-0dec86ec8b3b.json1")}"
+  credentials = "${file("../JasonM-ML-Project888-0dec86ec8b3b.json")}"
   project     = "jasonm-ml-project888"
   region      = "us-central1"
 }
